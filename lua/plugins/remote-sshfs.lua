154a309bbff89bb7cbabb7a8a2af2e67ce05a0cf
Nina Große <nina.grosse@posteo.de>
2025-01-09 18:30:15 +0100

    feat: install lazydocker, remote-sshfs, yazi plugins

---
+++ lua/plugins/remote-sshfs.lua
@@ -0,0 +1,19 @@
+return {
+  "nosduco/remote-sshfs.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    connections = {
+      sshfs_args = {
+        "-o follow_symlinks",
+      },
+    },
+    -- Refer to the configuration section below
+    -- or leave empty for defaults
+  },
+  init = function()
+    local api = require("remote-sshfs.api")
+    vim.keymap.set("n", "<leader>rc", api.connect, { silent = true, desc = "Remote connect" })
+    vim.keymap.set("n", "<leader>rd", api.disconnect, { silent = true, desc = "Remote disconnect" })
+    vim.keymap.set("n", "<leader>re", api.edit, { silent = true, desc = "Remote edit connection" })
+  end,
+}
