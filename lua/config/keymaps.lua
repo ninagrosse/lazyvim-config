262bf670664e1f1634ed633d189b901ab705e282
Nina Große <nina.grosse@posteo.de>
2025-01-09 18:29:29 +0100

    feat: change '<leader>l/L' keymaps to '<leader>ll/lL'

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+vim.keymap.set("n", "<leader>lL", function()
+  LazyVim.news.changelog()
+end, { desc = "LazyVim Changelog" })
+vim.keymap.del("n", "<leader>L")
