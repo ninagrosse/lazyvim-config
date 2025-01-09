866f65bb8f5acfc252f0d5e1724f2d7396859e9f
ngrosse <nina.grosse@riserid.eu>
2025-01-09 11:56:37 +0100

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+local transparent_background = true
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.94
+  vim.g.neovide_normal_opacity = 1
+  vim.g.neovide_cursor_animation_length = 0
+  transparent_background = false
+end
+
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = transparent_background, -- When in Neovide, this is set to false, otherwise true
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
