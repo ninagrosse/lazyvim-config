17ecc32cd4bf7345eace0272fc179e9e9121ab84
Nina Große <nina.grosse@posteo.de>
2025-05-12 12:25:48 +0200

    Revert "fix: pin mason to v1 because of breaking changes in v2"
    This reverts commit 324f0f5581a639b9bd3c889e04aab18c803314a3.

--- lua/plugins/mason-workaround.lua
+++
@@ -1,7 +0,0 @@
--- mason 2.0 has breaking changes, that are not compatible with LazyVim at the moment.
--- Pin mason to v1 to avoid the problem. Remove this file when LazyVim fixed it.
--- See https://github.com/LazyVim/LazyVim/issues/6039
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
