c622e5f80d10cf115890d4703c63963030246b27
ngrosse <nina.grosse@riserid.eu>
2025-07-29 11:45:22 +0200

    fix: new Neovide specific opacity settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 local transparent_background = true
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.94
+  vim.g.neovide_opacity = 0.94
   vim.g.neovide_normal_opacity = 1
   vim.g.neovide_cursor_animation_length = 0
   transparent_background = false
