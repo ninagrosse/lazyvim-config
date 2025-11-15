4702f5866e45e3272466975c03d1580126ba2185
Nina Große <nina.grosse@posteo.de>
2025-11-15 15:56:45 +0100

    feat: obsidian.nvim keymaps (WIP)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set("n", "<leader>lL", function()
   LazyVim.news.changelog()
 end, { desc = "LazyVim Changelog" })
 vim.keymap.del("n", "<leader>L")
+
+-- obsidian.nvim keymaps
+-- See commands: https://github.com/obsidian-nvim/obsidian.nvim
+vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian open<cr>", { desc = "Open in Obsidian" })
+vim.keymap.set("n", "<leader>ob", "<cmd>Obsidian backlinks<cr>", { desc = "Obsidian backlinks" })
