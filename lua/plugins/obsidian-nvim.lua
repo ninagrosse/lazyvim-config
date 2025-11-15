32ce8ec3badf269d0f67e4aa2add860f2872d915
Nina Große <nina.grosse@posteo.de>
2025-11-15 15:41:34 +0100

    feat: update obsidian.nvim and render-markdown.nvim settings
    - disable obsidian.nvim rendering, because it conflicts with
    render-markdown
    - update render-markdown to render obsidian checkbox toggles to match
    desktop app

--- lua/plugins/obsidian-nvim.lua
+++ lua/plugins/obsidian-nvim.lua
@@ -1,32 +1,51 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      preset = "obsidian",
+      code = {
+        sign = true,
+        width = "full",
+        right_pad = 0,
+      },
+      heading = {
+        sign = true,
+      },
+      checkbox = {
+        enabled = true,
+        checked = { scope_highlight = "@markup.strikethrough" },
+        custom = {
+          todo = { raw = "[~]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          alert = { raw = "[!]", rendered = "󰀦 ", highlight = "DiagnosticError", scope_highlight = nil },
+          arrow = { raw = "[>]", rendered = "󰒊 ", highlight = "DiagnosticWarn", scope_highlight = nil },
+          strike = { raw = "[-]", rendered = " ", scope_highlight = "@markup.strikethrough" },
+        },
+      },
+    },
+  },
+  {
     "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
+
     ---@module 'obsidian'
     ---@type obsidian.config
     opts = {
+      ui = {
+        enable = false,
+      },
       workspaces = {
         {
           name = "personal",
           path = "~/notebook",
         },
       },
-    },
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      preset = "obsidian",
+      checkbox = {
+        -- Checkboxen mit <CR> togglen
+        -- Wird von render-markdown gerendert
+        order = { " ", "~", "x", "!", ">", "-" },
+      },
     },
   },
 }
